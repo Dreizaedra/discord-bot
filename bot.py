2f13d5d1bf1dadc984f750e0a171845cc886fa32
Louis Arnault <louisarnault@gmail.com>
2024-02-21 23:24:53 +0100

    Change discord import

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 import os
-import discord
+from discord import Client, Message, Intents
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-class CustomClient(discord.Client):
+class CustomClient(Client):
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
 
-    async def on_message(self, message: discord.Message):
+    async def on_message(self, message: Message):
         if message.content == '/help':
             await message.channel.send(f'{message.author.mention} Here are my available commands:\n'
                                        + '**/help** - Show this message!\n'
@@ -18,7 +18,7 @@ class CustomClient(discord.Client):
             await message.channel.send(f'Hey {message.author.mention}!')
 
 
-intents = discord.Intents.default()
+intents = Intents.default()
 intents.typing = False
 intents.messages = True
 intents.message_content = True
